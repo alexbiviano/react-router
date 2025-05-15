304618465e59c2e599422c5e287c5fc06317e0b9
alexbiviano <bivianoalex63@gmail.com>
2025-05-15 16:01:21 +0200

    [upd] struttura base completa

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,1 +1,25 @@
 import { useState } from 'react'
+//Importo Routers
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+//Importo le pagine
+import Homepage from './pages/HomePage';
+import AboutUs from './pages/AboutUs';
+import Product from './pages/Product';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' Component={Homepage} />
+          <Route path='/product' Component={Product} />
+          <Route path='/about' Component={AboutUs} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ react-router/src/pages/Product.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+//Importo useState, useEffect
+import { useState, useEffect } from 'react'
+
+//Importo Navlink
+import { NavLink } from 'react-router-dom'
+
+//Importo Navbar
+import MainNavbar from '../components/MainNavbar'
+
+//Importo Axios
+import axios from 'axios'
+
+const Product = () => {
+    const [product, setProduct] = useState([])
+    const endpoint = 'https://fakestoreapi.com/products'
+
+    useEffect(() => {
+        axios.get(endpoint)
+            .then((response) => {
+                setProduct(response.data)
+            })
+    }, []);
+
+    return (
+        <div className="product-page">
+            <div className="header-container">
+                <h1 className="page-title">Products</h1>
+                <MainNavbar />
+            </div>
+
+            <div className="products-container">
+                {product.length === 0 ? (
+                    <div className="loading-container">Caricamento Prodotti...</div>
+                ) : (
+                    product.map((item) => (
+                        <div className="product-card" key={item.id}>
+                            <div className="product-image">
+                                <img src={item.image} alt={item.title} />
+                            </div>
+                            <div className="product-details">
+                                <h3 className="product-title">{item.title}</h3>
+                                <p className="product-price">${item.price}</p>
+                                <p className="product-description">{item.description}</p>
+                            </div>
+                        </div>
+                    ))
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Product
