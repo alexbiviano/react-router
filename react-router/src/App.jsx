0ae8f2f0aadea2f7ff8337ff5c4ce1ba5ecc68ae
alexbiviano <bivianoalex63@gmail.com>
2025-05-16 15:23:02 +0200

    [upd] create products

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import { React } from 'react'
+
 //Importo Routers
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 //Importo le pagine
-import Homepage from './pages/HomePage';
+import Homepage from './pages/Homepage';
 import AboutUs from './pages/AboutUs';
 import Product from './pages/Product';
-
+import DetailProduct from './pages/Product/DetailProduct';
+<pages />
 function App() {
 
   return (
@@ -14,7 +16,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' Component={Homepage} />
-          <Route path='/product' Component={Product} />
+          <Route path='/product'>
+            <Route index Component={Product} />
+            <Route path='/product/:id' Component={DetailProduct} />
+          </Route>
           <Route path='/about' Component={AboutUs} />
         </Routes>
       </BrowserRouter>
